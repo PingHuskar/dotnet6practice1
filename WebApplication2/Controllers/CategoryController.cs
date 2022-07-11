5a5a093370539a63dc6395c6b0bffed373fdc5e0
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-07-11 14:51:50 +0700

    first commit

---
+++ WebApplication2/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public CategoryController()
+        {
+
+        }
+        public ActionResult<string> Index()
+        {
+             return View();
+        }
+        public ActionResult<string> Edit()
+        {
+            return View("Index");
+        }
+        public ActionResult<string> Create(int id,string test)
+        {
+            //return id;
+            return Json(new { name = "Biw", Age = 30 });
+            //return View();
+        }
+    }
+}
---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var fullname = ("ชฎิล", "ชัยพรพิศุทธิ์");
+            ViewData["myname"] = fullname.Item1 + " " + fullname.Item2; 
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
